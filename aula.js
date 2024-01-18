59a8db8d087204c6fd427f76f5f02669a4c6a01a
maynarasanto <maynara999@gmail.com>
2024-01-17 21:01:41 -0300

    first commit

---
+++ aula.js
@@ -0,0 +1,30 @@
+//  let hora = 13;
+//  // resultado 
+
+//  //condiçao 
+// if (hora > 6 && hora < 12){
+//     console.log ("bom dia") // o que vai responder 
+// }
+// // else if mais de uma condiçao 
+// else if( hora >12 && hora <18){ 
+//     console.log ("boa tarde")
+// }
+// //else caso contrario
+// else {
+//     console.log("boa noite ")}
+
+    
+// escreva uma funçao que usa 2numero e retorne sempre o maior 
+
+// // a comparaçao
+// let valor = maior(10,5)
+
+// console.log(valor); //resposta
+
+// function maior(numero1,numero2)
+// {
+//     if(numero1 > numero2)
+//     return numero1;
+// else return numero2;
+// }
+
